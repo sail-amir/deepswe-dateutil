6d54bd47630514f4b0be4def2a11296d79b51196
sail-amir <71436956+sail-amir@users.noreply.github.com>
2026-09-01 12:33:43 +0800

    CodeScene global 20: isolate recurrence weekday parsing

--- src/dateutil/rrule.py
+++ src/dateutil/rrule.py
@@ -1493,27 +1493,28 @@ class _rrulestr(object):
         """
         Two ways to specify this: +1MO or MO(+1)
         """
-        l = []
-        for wday in value.split(','):
-            if '(' in wday:
-                # If it's of the form TH(+1), etc.
-                splt = wday.split('(')
-                w = splt[0]
-                n = int(splt[1][:-1])
-            elif len(wday):
-                # If it's of the form +1MO
-                for i in range(len(wday)):
-                    if wday[i] not in '+-0123456789':
-                        break
-                n = wday[:i] or None
-                w = wday[i:]
-                if n:
-                    n = int(n)
-            else:
-                raise ValueError("Invalid (empty) BYDAY specification.")
+        rrkwargs["byweekday"] = [self._parse_rfc_weekday(wday)
+                                 for wday in value.split(',')]
 
-            l.append(weekdays[self._weekday_map[w]](n))
-        rrkwargs["byweekday"] = l
+    def _parse_rfc_weekday(self, wday):
+        if '(' in wday:
+            # If it's of the form TH(+1), etc.
+            splt = wday.split('(')
+            w = splt[0]
+            n = int(splt[1][:-1])
+        elif len(wday):
+            # If it's of the form +1MO
+            for i in range(len(wday)):
+                if wday[i] not in '+-0123456789':
+                    break
+            n = wday[:i] or None
+            w = wday[i:]
+            if n:
+                n = int(n)
+        else:
+            raise ValueError("Invalid (empty) BYDAY specification.")
+
+        return weekdays[self._weekday_map[w]](n)
 
     _handle_BYDAY = _handle_BYWEEKDAY
 

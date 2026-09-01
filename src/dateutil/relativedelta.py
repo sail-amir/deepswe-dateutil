8817c3940d50a6daaa0d00b031ffe73b09269d4a
sail-amir <71436956+sail-amir@users.noreply.github.com>
2026-09-01 12:20:54 +0800

    CodeScene global 17: isolate relativedelta differences

--- src/dateutil/relativedelta.py
+++ src/dateutil/relativedelta.py
@@ -110,123 +110,123 @@ class relativedelta(object):
                  hour=None, minute=None, second=None, microsecond=None):
 
         if dt1 and dt2:
-            # datetime is a subclass of date. So both must be date
-            if not (isinstance(dt1, datetime.date) and
-                    isinstance(dt2, datetime.date)):
-                raise TypeError("relativedelta only diffs datetime/date")
+            self._set_difference(dt1, dt2)
+            self._fix()
+            return
 
-            # We allow two dates, or two datetimes, so we coerce them to be
-            # of the same type
-            if (isinstance(dt1, datetime.datetime) !=
-                    isinstance(dt2, datetime.datetime)):
-                if not isinstance(dt1, datetime.datetime):
-                    dt1 = datetime.datetime.fromordinal(dt1.toordinal())
-                elif not isinstance(dt2, datetime.datetime):
-                    dt2 = datetime.datetime.fromordinal(dt2.toordinal())
+        # Check for non-integer values in integer-only quantities
+        if any(x is not None and x != int(x) for x in (years, months)):
+            raise ValueError("Non-integer years and months are "
+                             "ambiguous and not currently supported.")
 
-            self.years = 0
-            self.months = 0
-            self.days = 0
-            self.leapdays = 0
-            self.hours = 0
-            self.minutes = 0
-            self.seconds = 0
-            self.microseconds = 0
-            self.year = None
-            self.month = None
-            self.day = None
-            self.weekday = None
-            self.hour = None
-            self.minute = None
-            self.second = None
-            self.microsecond = None
-            self._has_time = 0
+        # Relative information
+        self.years = int(years)
+        self.months = int(months)
+        self.days = days + weeks * 7
+        self.leapdays = leapdays
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+        self.microseconds = microseconds
 
-            # Get year / month delta between the two
-            months = (dt1.year - dt2.year) * 12 + (dt1.month - dt2.month)
-            self._set_months(months)
+        # Absolute information
+        self.year = year
+        self.month = month
+        self.day = day
+        self.hour = hour
+        self.minute = minute
+        self.second = second
+        self.microsecond = microsecond
 
-            # Remove the year/month delta so the timedelta is just well-defined
-            # time units (seconds, days and microseconds)
-            dtm = self.__radd__(dt2)
+        if any(x is not None and int(x) != x
+               for x in (year, month, day, hour,
+                         minute, second, microsecond)):
+            # For now we'll deprecate floats - later it'll be an error.
+            warn("Non-integer value passed as absolute information. " +
+                 "This is not a well-defined condition and will raise " +
+                 "errors in future versions.", DeprecationWarning)
 
-            # If we've overshot our target, make an adjustment
-            if dt1 < dt2:
-                compare = operator.gt
-                increment = 1
-            else:
-                compare = operator.lt
-                increment = -1
-
-            while compare(dt1, dtm):
-                months += increment
-                self._set_months(months)
-                dtm = self.__radd__(dt2)
-
-            # Get the timedelta between the "months-adjusted" date and dt1
-            delta = dt1 - dtm
-            self.seconds = delta.seconds + delta.days * 86400
-            self.microseconds = delta.microseconds
+        if isinstance(weekday, integer_types):
+            self.weekday = weekdays[weekday]
         else:
-            # Check for non-integer values in integer-only quantities
-            if any(x is not None and x != int(x) for x in (years, months)):
-                raise ValueError("Non-integer years and months are "
-                                 "ambiguous and not currently supported.")
+            self.weekday = weekday
 
-            # Relative information
-            self.years = int(years)
-            self.months = int(months)
-            self.days = days + weeks * 7
-            self.leapdays = leapdays
-            self.hours = hours
-            self.minutes = minutes
-            self.seconds = seconds
-            self.microseconds = microseconds
-
-            # Absolute information
-            self.year = year
-            self.month = month
-            self.day = day
-            self.hour = hour
-            self.minute = minute
-            self.second = second
-            self.microsecond = microsecond
-
-            if any(x is not None and int(x) != x
-                   for x in (year, month, day, hour,
-                             minute, second, microsecond)):
-                # For now we'll deprecate floats - later it'll be an error.
-                warn("Non-integer value passed as absolute information. " +
-                     "This is not a well-defined condition and will raise " +
-                     "errors in future versions.", DeprecationWarning)
-
-            if isinstance(weekday, integer_types):
-                self.weekday = weekdays[weekday]
+        yday = 0
+        if nlyearday:
+            yday = nlyearday
+        elif yearday:
+            yday = yearday
+            if yearday > 59:
+                self.leapdays = -1
+        if yday:
+            ydayidx = [31, 59, 90, 120, 151, 181, 212,
+                       243, 273, 304, 334, 366]
+            for idx, ydays in enumerate(ydayidx):
+                if yday <= ydays:
+                    self.month = idx+1
+                    if idx == 0:
+                        self.day = yday
+                    else:
+                        self.day = yday-ydayidx[idx-1]
+                    break
             else:
-                self.weekday = weekday
-
-            yday = 0
-            if nlyearday:
-                yday = nlyearday
-            elif yearday:
-                yday = yearday
-                if yearday > 59:
-                    self.leapdays = -1
-            if yday:
-                ydayidx = [31, 59, 90, 120, 151, 181, 212,
-                           243, 273, 304, 334, 366]
-                for idx, ydays in enumerate(ydayidx):
-                    if yday <= ydays:
-                        self.month = idx+1
-                        if idx == 0:
-                            self.day = yday
-                        else:
-                            self.day = yday-ydayidx[idx-1]
-                        break
-                else:
-                    raise ValueError("invalid year day (%d)" % yday)
+                raise ValueError("invalid year day (%d)" % yday)
 
         self._fix()
+
+    def _set_difference(self, dt1, dt2):
+        # datetime is a subclass of date. So both must be date
+        if not (isinstance(dt1, datetime.date) and
+                isinstance(dt2, datetime.date)):
+            raise TypeError("relativedelta only diffs datetime/date")
+
+        # We allow two dates, or two datetimes, so coerce them to the same type.
+        if (isinstance(dt1, datetime.datetime) !=
+                isinstance(dt2, datetime.datetime)):
+            if not isinstance(dt1, datetime.datetime):
+                dt1 = datetime.datetime.fromordinal(dt1.toordinal())
+            elif not isinstance(dt2, datetime.datetime):
+                dt2 = datetime.datetime.fromordinal(dt2.toordinal())
+
+        self.years = 0
+        self.months = 0
+        self.days = 0
+        self.leapdays = 0
+        self.hours = 0
+        self.minutes = 0
+        self.seconds = 0
+        self.microseconds = 0
+        self.year = None
+        self.month = None
+        self.day = None
+        self.weekday = None
+        self.hour = None
+        self.minute = None
+        self.second = None
+        self.microsecond = None
+        self._has_time = 0
+
+        # Get year / month delta between the two.
+        months = (dt1.year - dt2.year) * 12 + (dt1.month - dt2.month)
+        self._set_months(months)
+
+        # Remove the year/month delta so the remainder uses fixed time units.
+        dtm = self.__radd__(dt2)
+        if dt1 < dt2:
+            compare = operator.gt
+            increment = 1
+        else:
+            compare = operator.lt
+            increment = -1
+
+        while compare(dt1, dtm):
+            months += increment
+            self._set_months(months)
+            dtm = self.__radd__(dt2)
+
+        delta = dt1 - dtm
+        self.seconds = delta.seconds + delta.days * 86400
+        self.microseconds = delta.microseconds
 
     def _fix(self):
         if abs(self.microseconds) > 999999:
